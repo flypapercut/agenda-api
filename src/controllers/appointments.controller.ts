53852bf3914b57d19c85d6a056779f9a896036e5
flypapercut <flypapercut@proton.me>
2025-07-16 07:36:33 -0300

    add request params

--- src/controllers/appointments.controller.ts
+++ src/controllers/appointments.controller.ts
@@ -1,17 +1,19 @@
 import {Request, Response, NextFunction} from "express"
 
 class AppointmentsController{
-
+  // returns all appointments
   index(request:Request, response:Response, next:NextFunction){
     console.log("index")
 
     return response.status(200).send("index")
   }
-
+  // returns appointments of certain dates
   indexByDate(request:Request, response:Response, next:NextFunction){
     console.log("indexByDate")
 
-    return response.status(200).send("index DATE")
+    const date = request.params.date
+
+    return response.status(200).send(`index date ${date}`)
   }
 
 }
--- src/validator/appointment.ts
+++
@@ -1,8 +0,0 @@
-import {z} from "zod"
-
-export const appointmentSchema = z.object({
-  title: z.string().min(1),
-  description: z.string().optional(),
-  date: z.string(),
-  time: z.string().optional()
-})
