b783873735cdef73eb8cbaa03badbc9228942456
flypapercut <flypapercut@proton.me>
2025-07-16 09:13:57 -0300

    add create and index functionality

--- src/controllers/appointments.controller.ts
+++ src/controllers/appointments.controller.ts
@@ -1,12 +1,27 @@
 import {Request, Response, NextFunction} from "express"
+import {appointmentSchema} from "../validators/appointmentSchema"
+import {knexConfig as knex} from "@/database/knex"
+import { Appointment } from "@/models/Appointment"
+
+import {v4 as uuidv4} from "uuid"
 
 class AppointmentsController{
   // returns all appointments
-  index(request:Request, response:Response, next:NextFunction){
-    console.log("index")
+  async index(request:Request, response:Response, next:NextFunction){  
+    try {
+    
+      console.log("index")
 
-    return response.status(200).send("index")
+      const appointments = await knex<Appointment>("appointments").select()
+
+      return response.status(200).json(appointments)
+    }
+    catch (error) {
+      next(error)
+    }
   }
+
+
   // returns appointments of certain dates
   indexByDate(request:Request, response:Response, next:NextFunction){
     console.log("indexByDate")
@@ -14,6 +29,25 @@ class AppointmentsController{
     const date = request.params.date
 
     return response.status(200).send(`index date ${date}`)
+  }
+
+  async create(request:Request, response:Response, next:NextFunction){
+    try {
+      
+      
+
+      const {title, description, date, time} = appointmentSchema.parse(request.body)
+
+      const id = uuidv4()
+
+      //await knex<AppointmentRepository>("appointments").insert({id, title, description, date, time})
+      await knex<Appointment>("appointments").insert({id, title, description, date, time})
+
+      return response.status(201).json()
+    } 
+    catch (error) {
+      next(error)
+    }
   }
 
 }
