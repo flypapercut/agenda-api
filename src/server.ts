068e6739256fe714aca4a4ddcd30832fa0744444
flypapercut <flypapercut@proton.me>
2025-07-27 22:31:52 -0300

    add cors package

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 import {routes} from "./routes"
 import { errorHandling } from "./middlewares/error_handling"
@@ -8,11 +9,21 @@ const app = express()
 
 app.use(express.json())
 
+app.use(cors())
 
+// cors
+// app.use((req, res, next)=>{
+//   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next()
+// })
+//
 
 
 app.use(routes)
 
 app.use(errorHandling)
+
+
 
 app.listen(3333, () => {console.log("running agenda on port 3333")})
