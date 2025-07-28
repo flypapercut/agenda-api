3207e3bc66bbd1931cfa4c9fc213466ceb3fc60d
flypapercut <flypapercut@proton.me>
2025-07-27 22:34:12 -0300

    remove old cors usage

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,6 @@ app.use(express.json())
 
 app.use(cors())
 
-// cors
-// app.use((req, res, next)=>{
-//   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next()
-// })
-//
-
 
 app.use(routes)
 
